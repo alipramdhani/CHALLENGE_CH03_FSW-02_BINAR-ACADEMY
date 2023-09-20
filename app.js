8752c80a0bc589005d1abaed2935fbc602ce9917
unknown <aliframdani279@gmail.com>
2023-09-21 02:12:34 +0700

    Merapihkan struktur code CRUD REST API data cars

--- app.js
+++ app.js
@@ -1,145 +1,194 @@
-// CORE PACKAGE/MODULE
-const fs = require('fs');
-
-
-// THIRD PACKAGE/MODULE
-// const http = require('http');
-const express = require('express');
+const fs = require("fs")
+const express = require("express")
+const morgan = require("morgan")
 const app = express()
 
 // middleware dari express
-// memodifikasi incoming request/request body ke api kita
-app.use(express.json());
+app.use(express.json())
+app.use(morgan("dev"))
+
+// OUR OWN MIDDLEWARE
+// logging
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+    console.log(req.requestTime)
+    next()
+})
 
 // bikin env.port untuk membaca env port atau port 3000
-const port = process.env.port || 8000;
+const port = process.env.port || 8000
 
 // baca data dari file json
-const cars = JSON.parse(fs.readFileSync(`${__dirname}/data/cars.json`));
+const cars = JSON.parse(
+    fs.readFileSync(`${__dirname}/data/cars.json`)
+)
 
 // a. GET / akan membuka root endpoint dengan response {message: “ping successfully”}.
-// Basic routing endpoint
-app.get('/', (req, res) => {
+
+const ping = (req, res) => {
+    // Basic routing endpoint
     res.status(200).json({
         message: "ping successfuly",
     })
-})
+}
 
 // b. GET /cars akan membuka list cars
 // Menampilkan semua list cars
-app.get('/api/v1/cars', (req, res) => {
+const getAllcars = (req, res) => {
     res.status(200).json({
-        status: 'success',
+        status: "success",
+        requestTime: req.requestTime,
         data: {
-            cars
-        }
+            cars,
+        },
     })
-})
+}
 
 // c. GET /cars/:id akan membuka satu data cars.
 // Menampilkan satu data cars menggunakan ID
-app.get('/api/v1/cars/:id', (req, res) => {
-    const carId = req.params.id;
+const getCarsById = (req, res) => {
+    const carId = req.params.id
     // Cari mobil berdasarkan ID
-    const car = cars.find(el => el.id === carId)
+    const car = cars.find((el) => el.id === carId)
 
     // Jika mobil ditemukan, kirimkan sebagai respons
-    if(car) {
+    if (car) {
         res.status(200).json({
-            status: 'success',
+            status: "success",
             data: {
-                car
-            }
+                car,
+            },
         })
     } else {
         // Jika mobil tidak ditemukan, kirimkan respons 404 Not Found
         return res.status(404).json({
-            status: 'failed',
-            message: `car with id ${carId} this not found`
+            status: "failed",
+            message: `car with id ${carId} this not found`,
         })
-    } 
-});
+    }
+}
 
 // d. POST /cars akan mengembalikan response data cars yang sudah terbuat.
 // Menambahkan data cars menggunakan endpoint POST
-app.post('/api/v1/cars', (req, res) => {
+const createCars = (req, res) => {
     // generate id untuk data baru request api kita
-    const newId = cars[cars.length - 1].id +1;
-    const newCar = Object.assign({id: newId}, req.body);
-
-    cars.push(newCar);
-    fs.writeFile(`${__dirname}/data/cars.json`,
-    JSON.stringify(cars), 
-    err => { 
-        res.status(201).json({
-            status: 'success',
-            data: {
-                    car: newCar
+    const newId = cars[cars.length - 1].id + 1
+    const newCar = Object.assign(
+        { id: newId },
+        req.body
+    )
+    // Menambahkan mobil baru ke array 'cars'
+    cars.push(newCar)
+    fs.writeFile(
+        `${__dirname}/data/cars.json`,
+        JSON.stringify(cars),
+        (err) => {
+            res.status(201).json({
+                status: "success",
+                data: {
+                    car: newCar,
                 },
-            });
-         }
-    );
-    // res.send('udah');
-});
+            })
+        }
+    )
+}
 
 // d. POST /cars akan mengembalikan response data cars yang sudah terbuat.
 // Mengupdate data cars menggunakan endpoint PUT
-app.put('/api/v1/cars/:id',(req, res) => {
-    const carId = req.params.id;;
+const updateCars = (req, res) => {
+    const carId = req.params.id
     // findIndex = -1 (kalau data nya gk ada)
-    const carIndex = cars.findIndex(el => el.id === carId)
+    const carIndex = cars.findIndex(
+        (el) => el.id === carId
+    )
 
-    if(!carIndex === -1) {
+    // Memeriksa apakah mobil dengan ID yang diberikan ditemukan
+    if (!carIndex === -1) {
         return res.status(404).json({
-            status: 'failed',
-            message: `data with ${carId} this not found`
+            status: "failed",
+            message: `data with ${carId} this not found`,
         })
     }
-    cars[carIndex] = {...cars[carIndex], ...req.body}
 
-    fs.writeFile(`${__dirname}/data/cars.json`,
-    JSON.stringify(cars), 
-    err => {
-        res.status(200).json({
-            status: 'success',
-            message: `car with this id ${carId} edited`,
-            data: {
-                    car: cars[carIndex]
-            },
-        });
-    })
-})
+    // Jika mobil ditemukan, mengganti data mobil dengan data dari permintaan (request)
+    cars[carIndex] = {
+        ...cars[carIndex],
+        ...req.body,
+    }
+
+    // Menyimpan perubahan data mobil ke dalam file JSON
+    fs.writeFile(
+        `${__dirname}/data/cars.json`,
+        JSON.stringify(cars),
+        (err) => {
+            res.status(200).json({
+                status: "success",
+                message: `car with this id ${carId} edited`,
+                data: {
+                    car: cars[carIndex],
+                },
+            })
+        }
+    )
+}
 
 // f. DELETE /cars/:id akan mengembalikan response data cars yang sudah terhapus.
 // Menghapus data cars menggunakan endpoint DELETE
-app.delete('/api/v1/cars/:id',(req, res) => {
+const removeCars = (req, res) => {
     // konversi string jadi number
-    const carId = req.params.id;
+    const carId = req.params.id
     // cari index dari data yang sesuai id di req.params
-    const carIndex = cars.findIndex(el => el.id === carId);
+    const carIndex = cars.findIndex(
+        (el) => el.id === carId
+    )
 
     // validasi kalau data yang sesuai req.param id nya ga ada
-    if(carIndex === -1) {
+    if (carIndex === -1) {
         return res.status(404).json({
-            status: 'failed',
-            message: 'data not found'
+            status: "failed",
+            message: "data not found",
         })
     }
     // proses menghapus data sesuai index array nya dari req.param.id
     cars.splice(carIndex, 1)
 
     // proses update di file json nya
-    fs.writeFile(`${__dirname}/data/cars.json`,
-    JSON.stringify(cars), 
-    err => {
-        res.status(200).json({
-            status: 'success',
-            message: 'berhasil delete data',
-            data: null
-        });
-    })
-})
+    fs.writeFile(
+        `${__dirname}/data/cars.json`,
+        JSON.stringify(cars),
+        (err) => {
+            res.status(200).json({
+                status: "success",
+                message: "berhasil delete data",
+                data: null,
+            })
+        }
+    )
+}
+// Membuat router khusus untuk rute '/test'
+const testRouter = express.Router()
+// Membuat router khusus untuk rute '/api/v1/cars'
+const carRouter = express.Router()
+//  Menyambungkan rute '/test' dengan fungsi 'ping' saat rute tersebut diakses dengan metode GET
+testRouter.route("/").get(ping)
 
+carRouter
+    .route("/")
+    .get(getAllcars) // Mendapatkan semua data mobil
+    .post(createCars) // Membuat data mobil baru
+
+carRouter
+    .route("/:id")
+    .get(getCarsById) // Mendapatkan data mobil berdasarkan ID
+    .put(updateCars) // Mengubah data mobil berdasarkan ID
+    .delete(removeCars) // Menghapus data mobil berdasarkan ID
+
+// Menggunakan router '/test' untuk rute beranda ('/')
+app.use("/", testRouter)
+// Menggunakan router '/api/v1/cars' untuk rute yang berkaitan dengan mobil
+app.use("/api/v1/cars", carRouter)
+
+// Mulai server Express.js
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-});
+    console.log(`App running on port ${port}...`)
+})
